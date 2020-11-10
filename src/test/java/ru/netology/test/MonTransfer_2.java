5fc4409c733c8ba90a272ae1b070cd3e440c4af8
Sergey K <89184083452.mail.ru>
2020-11-11 00:48:20 +0300

    другой вапиант

--- src/test/java/ru/netology/test/MonTransfer_2.java
+++ src/test/java/ru/netology/test/MonTransfer_2.java
@@ -3,6 +3,8 @@ package ru.netology.test;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelp;
+import ru.netology.page.DashboardCardPage;
+import ru.netology.page.DashboardCardReplPage;
 import ru.netology.page.LoginPage_2;
 import ru.netology.page.VerificationPage_2;
 
@@ -18,7 +20,10 @@ public class MonTransfer_2 {
         loginPage.validLogin(log_Pass);
         var verPage = new VerificationPage_2();
         verPage.validVerify(DataHelp.getVerificationCodeFor(log_Pass));
-//        var verCode_SMS = DataHelp.getVerificationCodeFor(log_Pass);
-//        verPage.validVerify(verCode_SMS);
+        var dashboardCardPage = new DashboardCardPage();
+        dashboardCardPage.replenishCard(1);
+        DashboardCardReplPage dashboardCardReplPage = new DashboardCardReplPage();
+        dashboardCardReplPage.replenishCard("456", 1);
+        System.out.println("Баланс 1 карты =" + dashboardCardPage.infoBalansCard(1) + " руб.");
     }
 }
